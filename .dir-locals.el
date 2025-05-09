7f4374654feeee2b2c207d3f23c4dec7596c6857
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2025-05-09 11:41:54 +0100

    removed .dir-locals.el

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-((nil . ((eval . (progn
-                   ;; Load the configuration script
-                   (load-file "goto-transcoder-mode.el")
-                   ;; Enable the `goto-transcoder` minor mode
-                   (goto-transcoder-mode 1))))))
