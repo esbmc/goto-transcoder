e2815b5977b5b79fb2687c6eec6d330bbaac0162
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2026-02-23 18:06:36 +0000

    new issue tracker (#6)

--- goto-transcoder-mode.el
+++ goto-transcoder-mode.el
@@ -1,11 +1,52 @@
-;;; goto_transcoder-mode.el --- Defines the elisp functions for this project -*- lexical-binding: t -*-
+;;; goto-transcoder-mode.el --- Defines the elisp functions for this project -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
+
+(require 'org)
+(require 'org-capture)
+
+(defvar goto-transcoder-project-root nil
+  "Root directory of the goto-transcoder project.")
+
+(defun goto-transcoder--todo-file ()
+  "Return the path to the project TODO.org."
+  (when goto-transcoder-project-root
+    (expand-file-name "TODO.org" goto-transcoder-project-root)))
+
+(defun goto-transcoder--next-id ()
+  "Return the next CUSTOM_ID for a new issue."
+  (let ((max-id 0)
+        (todo-file (goto-transcoder--todo-file)))
+    (when (and todo-file (file-exists-p todo-file))
+      (with-current-buffer (find-file-noselect todo-file)
+        (org-map-entries
+         (lambda ()
+           (let ((id (org-entry-get nil "CUSTOM_ID")))
+             (when id
+               (setq max-id (max max-id (string-to-number id)))))))))
+    (number-to-string (1+ max-id))))
 
 (defun goto-transcoder-setup ()
   "Set up the project main commands for use with project."
   (message "GOTO Transcoder Mode Enabled")
-  (setq-local compile-command "cargo build"))
+  (setq-local compile-command "cargo build")
+  (let ((root (locate-dominating-file default-directory "TODO.org")))
+    (when root
+      (setq goto-transcoder-project-root root)
+      (let ((todo-file (expand-file-name "TODO.org" root)))
+        (unless (member todo-file org-agenda-files)
+          (add-to-list 'org-agenda-files todo-file))
+        (let ((template
+               `("g" "GOTO Transcoder Issue" entry
+                 (file ,todo-file)
+                 ,(concat "* TODO [#B] %?\n"
+                          ":PROPERTIES:\n"
+                          ":CUSTOM_ID: %(goto-transcoder--next-id)\n"
+                          ":CREATED: %U\n"
+                          ":END:\n")
+                 :empty-lines 1)))
+          (unless (assoc "g" org-capture-templates)
+            (add-to-list 'org-capture-templates template)))))))
 
 (defun goto-transcoder-test ()
   "Run all test cases from the project."
@@ -16,10 +57,8 @@
   "A minor mode for the goto-transcoder features."
   :lighter " GotoTranscoder"
   :keymap (let ((map (make-sparse-keymap)))
-            ;; Define keybindings here
             (define-key map (kbd "C-x p t") 'goto-transcoder-test)
             map)
-  ;; Code to run when the mode is activated
   (if goto-transcoder-mode (goto-transcoder-setup)
     ()))
 
